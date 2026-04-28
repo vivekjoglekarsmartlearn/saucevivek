947d02c2858871c511c4e260e93e7ced4b9f315b
Vivek <saishraddha4@gmail.com>
2026-04-28 23:59:20 +0530

    second

---
+++ src/test/java/testcase/Login1.java
@@ -0,0 +1,35 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.vivek.testbase.TestBase;
+import com.vivek.testutil.TestUtil;
+
+public class Login1  extends  TestBase {
+
+@Test(priority =1)
+public void logintest()
+{
+	System.out.println("Vivek");
+	
+}
+
+//@AfterMethod//  exeucute after test		
+//public void  tearDown() { 		
+//		//quit browser
+//		driver.quit();
+//		
+//}
+//		
+
+	}
+
+
